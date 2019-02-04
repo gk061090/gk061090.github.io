97ab5e87167ee291a7da49815965bcf0b06879a3
Grigory Kuimov <gk061090@gmail.com>
2019-02-04 23:29:10 +0300

    prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,15 +4,15 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const safeParser = require("postcss-safe-parser");
-const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+//const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
 
 module.exports = {
   mode: "production",
   entry: path.join(__dirname, "src/js/index.js"),
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].[chunkhash:8].js",
-    publicPath: "/uploads/landing/302a75ca-e6d8-4e8a-8624-47f001dc83f1/"
+    path: path.join(__dirname, "/"),
+    filename: "dist/js/[name].js",
+    publicPath: "/"
   },
   optimization: {
     minimizer: [
@@ -54,26 +54,18 @@ module.exports = {
             use: ["pug-loader"]
           },
           {
-            test: [/\inline.*.bmp$/, /\inline.*.gif$/, /\inline.*.jpe?g$/, /\inline.*.png$/, /\inline.*.svg$/, /\.woff2$/, /\.woff$/],
-            loader: "url-loader",
-            options: {
-              limit: 9999999,
-              name: "img/[name].[hash:8].[ext]"
-            }
-          },
-          {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
             loader: "url-loader",
             options: {
-              limit: 30000,
-              name: "img/[name].[hash:8].[ext]"
+              limit: 100,
+              name: "dist/img/[name].[hash:8].[ext]"
             }
           },
           {
             loader: "file-loader",
             exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             options: {
-              name: "[name].[hash:8].[ext]"
+              name: "dist/js/[name].[ext]"
             }
           }
         ]
@@ -82,13 +74,11 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash:8].css"
+      filename: "dist/css/[name].css"
     }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src/index.pug"),
-      minify: true,
-      inlineSource: ".(css)$"
+      minify: true
     }),
-    new HtmlWebpackInlineSourcePlugin()
   ]
 };
